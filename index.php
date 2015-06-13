6fb0a849ddbc6015983a13b7bb7fb2f9593e33fe
Carlos Montezano <cmontezanol@gmail.com>
2015-06-13 02:31:35 -0300

    created a common script init.php to act as setup

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
-
 // framework/index.php
 
-require_once __DIR__ . '/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGLobals();
+require_once __DIR__ . '/init.php';
 
 $input = $request->get('name', 'World');
 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->send();
